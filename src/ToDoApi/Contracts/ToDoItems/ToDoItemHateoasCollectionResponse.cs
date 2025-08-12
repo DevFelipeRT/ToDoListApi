5f73d122744dd3fcf5e60104fadbfc77261477c7
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-08-12 16:02:33 -0300

    refactor: Remove unused ToDo item request and response contracts

--- src/ToDoApi/Contracts/ToDoItems/ToDoItemHateoasCollectionResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace ToDoList.ToDoApi.Contracts.ToDoItems;
-
-public sealed class ToDoItemHateoasCollectionResponse
-{
-    public List<ToDoItemHateoasResponse> Items { get; set; } = new();
-    public List<LinkDto> Links { get; set; } = new();
-}
