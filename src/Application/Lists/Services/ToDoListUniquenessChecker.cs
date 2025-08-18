b2f69bcdf68c34df619db773a1f8e74fdf313f54
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-08-18 15:07:22 -0300

    refactor: renamed service

--- src/Application/Lists/Services/ToDoListUniquenessChecker.cs
+++ src/Application/Lists/Services/ToDoListUniquenessChecker.cs
@@ -2,32 +2,32 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain.Accounts.ValueObjects;
-using Domain.Lists.Repositories;
-using Domain.Lists.Services.Interfaces;
 using Domain.Lists.ValueObjects;
+using Domain.Lists.Repositories;
+using Domain.Lists.Policies;
 
 namespace Application.Lists.Services;
 
 /// <summary>
-/// Provides functionality to check the uniqueness of a to-do list title for a given user.
+/// Provides functionality to check the uniqueness of a to-do list for a given account.
 /// </summary>
-public class ToDoListTitleUniquenessChecker : IToDoListTitleUniquenessChecker
+public class ToDoListUniquenessChecker : IToDoListUniquenessPolicy
 {
     private readonly IToDoListRepository _repository;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ToDoListTitleUniquenessChecker"/> class.
+    /// Initializes a new instance of the <see cref="ToDoListUniquenessChecker"/> class.
     /// </summary>
     /// <param name="repository">The to-do list repository to access persisted lists.</param>
-    public ToDoListTitleUniquenessChecker(IToDoListRepository repository)
+    public ToDoListUniquenessChecker(IToDoListRepository repository)
     {
         _repository = repository;
     }
 
     /// <inheritdoc />
-    public async Task<bool> IsTitleUniqueAsync(AccountId userId, Title title, CancellationToken cancellationToken)
+    public async Task<bool> IsTitleUniqueAsync(AccountId accountId, Title title, CancellationToken cancellationToken)
     {
-        var lists = await _repository.GetAllByUserAsync(userId, cancellationToken);
+        var lists = await _repository.GetAllByAccountAsync(accountId, cancellationToken);
 
         // Title equality comparison is handled by the VO (case-insensitive, trimmed)
         return !lists.Any(list => list.Title == title);
