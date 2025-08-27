8967e5f97dcd60afed74a46914df902d6bf4ddbb
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-08-27 13:57:07 -0300

    refactor: remove NoOpEmailSender implementation

--- src/Infrastructure/Email/NoOpEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using Application.Notifications.Email;
-
-namespace Infrastructure.Email;
-
-public sealed class NoOpEmailSender : IEmailSender
-{
-    public Task SendAsync(EmailMessage message, CancellationToken cancellationToken = default)
-        => Task.CompletedTask;
-}
