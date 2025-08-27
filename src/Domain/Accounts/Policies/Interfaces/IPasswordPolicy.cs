9cba3837039089f7a4d4e3d35a7d1056f277b127
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-08-27 13:56:35 -0300

    refactor: remove unused account lockout and password policy interfaces and implementations

--- src/Domain/Accounts/Policies/Interfaces/IPasswordPolicy.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Accounts.Policies.Interfaces;
-
-/// <summary>
-/// Service that validates password complexity policies.
-/// </summary>
-public interface IPasswordPolicy
-{
-    bool IsValid(string password, out string? reason);
-}
